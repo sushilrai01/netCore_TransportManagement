819907b9b4842fb0bc4b6c691c2fc3d8c8c44f10
sushilrai01 <ssilray16@gmail.com>
2023-09-18 18:07:19 +0545

    Create option for transport

---
+++ Controllers/TransportController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netCoreTransportMgmt.Entity;
+using netCoreTransportMgmt.ViewModel;
+
+namespace netCoreTransportMgmt.Controllers
+{
+   
+    public class TransportController : Controller
+    {
+        private readonly TransportManagementContext db;
+        public TransportController(TransportManagementContext db)
+        {
+            this.db = db;       
+        }
+
+        //GET: Transport/Index
+        public async Task<IActionResult> Index()
+        {
+            
+            return View();
+        }
+
+        //GET: Transport/Create
+        public IActionResult Create()
+        {
+            TransportRouteModel model = new TransportRouteModel();
+            model.TypeList = db.TypeDetails.Select(x => new DropDownModel { ID = x.TypeId, Text = x.Name }).ToList();
+            model.RouteList = db.RouteDetails.Select(x => new DropDownModel { ID = x.RouteId, Text = x.Origin + " To " + x.Destination }).ToList();
+            model.DriverList = db.DriverDetails.Select(x => new DropDownModel { ID = x.DriverId, Text = x.Name }).ToList();
+            return View(model); 
+        }
+    }
+}
